a68d31dfea5577ff04a600fa8a6ca0a13130f8f2
JhonRobert20 <jhonbara51214@gmail.com>
2021-03-17 18:28:18 +0100

    pipeline

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
-console.log("Hola Dimitri");
-console.log("blabla");
+/*Forma un equip amb 2 desenvolupadors més que estiguin en aquest mateix Sprint (el mentor pot ajudar-te o suggerir-te companys).
+Haureu de construir el projecte TO-DO. Ha de satisfer els següents requisits:
+
+Creeu una aplicació que permeti portar un llistat de tasques per fer. Ha de contemplar l'opció d'afegir tasques, llistar-les i mostrar el seu 
+estat (pendents, en execució o acabades), i l'hora d'inici i fi de la tasca, així com l'usuari que la va donar d'alta
+S'ha d'utilitzar per línia de comandos, i ha de contenir les següents opcions: crear tasca, actualitzar tasca, esborrar tasca, llistar 
+totes les tasques o llistar una tasca específica
+Heu d'utilitzar un repositori github seguint la seqüència gitflow */
+
+
+/* crear tasca, actualizar tasca(jhon) / borrar tasca, llistar (anastasia) / frontend (dani)
+/ Crear: rellenar datos, tambien fecha o opcion de crear con esa fecha concreta, si se sabe poner fecha de fin
+/ Actualizar lo mismo
+{
+
+    "tasks":[
+            {
+                "id": "", automatico
+                "user":"", requerido
+                "description": "", requerido
+                "date_start":"YES", no requerido(por defecto en el momento de crear)
+                "date_end": {"NO":[]} no requerido
+            },
+            {
+                "id": "",
+                "user":"",
+                "description": "",
+                "date_start":"YES",
+                "date_end": {"NO":[]}
+            }
+        ]
+        
+}
+
+
+*/
