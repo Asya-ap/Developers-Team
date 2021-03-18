3643175ae6cee90b8a1daad857a4c32d821d8980
Daniel <daniel.macias.gomez@gmail.com>
2021-03-18 13:03:54 +0100

    Stable frontend

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const rls = require("readline-sync");
+
+let appOn = true;
+
+while (appOn) {
+    const username = rls.question("Welcome to the TO-DO list app!\nWhat is your username?\n");
+    console.log(`Hi ${username}!`);
+
+    appOn = false;
+}
